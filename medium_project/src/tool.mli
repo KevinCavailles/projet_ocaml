e0f35908b2723fdfeb7523f55d243504374862d4
kevin <kcavaill@etud.insa-toulouse.fr>
2020-11-24 21:57:38 +0100

    acceptable_project clean

--- medium_project/src/tool.mli
+++
@@ -1,9 +0,0 @@
-open Graph
-
-(* Clone a graph by keeping only its nodes *)
-val clone_nodes: 'a graph -> 'b graph
-
-(* Apply a function f to every label of the graph's arcs *)
-val gmap: 'a graph -> ('a -> 'b) -> 'b graph
-
-val add_arc: float graph -> id -> id -> float -> float graph
